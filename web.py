c226a11d31e76e5cfa108e2fcbf34d56177c4de4
MaxGoodnic <89256159+MaxGoodnic@users.noreply.github.com>
2023-05-16 12:34:57 +0300

    Update web.py

--- web.py
+++ web.py
@@ -1,8 +1,5 @@
 from apikey import API_TOKEN
 
-# from pprint import PrettyPrinter
-
-# pp=PrettyPrinter()
 url = 'https://api.edamam.com/api/recipes/v2'
 params = {'type': 'public', 'app_id': 'd1fe2d37', 'app_key': API_TOKEN, 'mealType': ''}
 all_links = []
