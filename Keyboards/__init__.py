7e85d33c90d3e7a56cacac5fe378bf71f8decc3b
MaxGoodnic <89256159+MaxGoodnic@users.noreply.github.com>
2023-05-16 13:46:29 +0300

    Update __init__.py

--- Keyboards/__init__.py
+++ Keyboards/__init__.py
@@ -1,1 +1,1 @@
-from Keyboards.client_kb import kb_begin,kb_meal_type, kb_add, kb_country_type, kb_coctail_type
+from Keyboards.client_kb import kb_begin, kb_meal_type, kb_add, kb_country_type, kb_coctail_type
