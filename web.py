d0017929d4c5fde13b0046b7e7cd6d3d6f5484c9
MaxGoodnic <89256159+MaxGoodnic@users.noreply.github.com>
2023-05-16 15:12:35 +0300

    Update web.py

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from apikey import API_TOKEN
+from config.config import API_ID
 
 url = 'https://api.edamam.com/api/recipes/v2'
-params = {'type': 'public', 'app_id': 'd1fe2d37', 'app_key': API_TOKEN, 'meal_type': ''}
+params = {'type': 'public', 'app_id': API_ID, 'app_key': API_TOKEN, 'meal_type': ''}
 all_links = []
